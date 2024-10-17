69a4e912d7cb8940d98765d698f097dc921f0e35
Camille Piponiot <camille.piponiot@gmail.com>
2024-10-17 10:06:49 +0200

    fix lint error

--- r/interpolate.R
+++ r/interpolate.R
@@ -1,6 +1,6 @@
-#' Function to linearly interpolate missing diameter values for one stem. Returns a
-#' data.frame with all diameters and years, including interpolated values (if
-#' any).
+#' Function to linearly interpolate missing diameter values for one stem.
+#' Returns a data.frame with all diameters and years, including interpolated
+#' values (if any).
 #'
 #' @param diam stem's measured diameters in cm.
 #' @param years census years corresponding to diameter measurements.
@@ -10,8 +10,8 @@ interpolate <- function(diam, years, all_years) {
   # add all measurement years
   df <- data.frame(diam, year = years) |>
     merge(data.frame(year = all_years), all = TRUE) |>
-    # remove NA values before the first of after the last measured diameter
-    subset(!(is.na(diam) & (year < min(years) | year > max(years))))
+    # remove values before the first of after the last measured diameter
+    subset(year >= min(years) & year <= max(years))
   # interpolate NA values
   df$diam <- approx(df$year, df$diam, df$year)$y
   return(df)
