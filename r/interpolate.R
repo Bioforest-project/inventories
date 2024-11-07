17b20a0d82d432f43fc40b5dc1826123eb7533b5
Camille Piponiot <camille.piponiot@gmail.com>
2024-11-07 09:57:12 +0100

    add missing dbh interpolation

--- r/interpolate.R
+++ r/interpolate.R
@@ -1,18 +1,13 @@
 #' Function to linearly interpolate missing diameter values for one stem.
-#' Returns a data.frame with all diameters and years, including interpolated
-#' values (if any).
+#' Returns a vector with all interpolated diameters (if any).
 #'
 #' @param diam stem's measured diameters in cm.
 #' @param years census years corresponding to diameter measurements.
-#' @param all_years complete list of census years in the plot to which the stem
 #'   belongs.
-interpolate <- function(diam, years, all_years) {
-  # add all measurement years
-  df <- data.frame(diam, year = years) |>
-    merge(data.frame(year = all_years), all = TRUE) |>
-    # remove values before the first of after the last measured diameter
-    subset(year >= min(years) & year <= max(years))
-  # interpolate NA values
-  df$diam <- approx(df$year, df$diam, df$year)$y
-  return(df)
+interpolate <- function(diam, years) {
+  if (sum(!is.na(diam)) > 1) {
+    return(approx(years, diam, years)$y)
+  } else {
+    return(diam)
+  }
 }
