b36175129a80bd0d070cc24d7093601cd2ea8738
mithilaunkule <mithilaunkule1@gmail.com>
2025-03-12 17:48:03 +0100

    changing cache check to csv instead of pdf report Adding "try" to identify failed sites, and keep the code running Keeping the individual site files eafter generating aggregated data file

--- analyses/automate_compilation.R
+++ analyses/automate_compilation.R
@@ -25,7 +25,7 @@ in_plot_info <- read.csv("data/raw_data/bioforest-plot-information.csv", fileEnc
 compile_sites <- intersect(raw_data_sites, c("tene2018", in_plot_info))
 
 #removing sites that are not working 
-remove <- c( "nelliyampathy")
+remove <- c( "mil")
 compile_sites <- compile_sites[!compile_sites %in% remove] 
 
 # cache: if we don't want to redo the compilation for files that already exist
@@ -33,36 +33,46 @@ compile_sites <- compile_sites[!compile_sites %in% remove]
 cache <- TRUE
 
 if (cache) {
-  done <- list.files("reports/") |>
-    gsub(pattern = "data_aggregation_|.pdf", replacement = "")
-
+  #done <- list.files("reports/") |>
+  #  gsub(pattern = "data_aggregation_|.pdf", replacement = "")
+  done <- list.files("data/derived_data/") |>
+    gsub(pattern = "aggregated_data_|.csv", replacement = "")
   compile_sites <- setdiff(compile_sites, done)
 }
 
+
+failed_sites <- c()
 if (length(compile_sites) > 0) {
   for (s in compile_sites) {
     # render document
-    quarto::quarto_render(
-      input = "analyses/data_aggregation.qmd",
-      output_format = "all",
-      output_file = paste0("data_aggregation_", s, ".pdf"),
-      execute_params = list(site = s, taper = FALSE)
-    )
-    # move to "outputs/data_aggregation_reports" folder
-    file.rename(
-      from = paste0("data_aggregation_", s, ".pdf"),
-      to = paste0("reports/data_aggregation_", s, ".pdf")
-    )
+    error <- try({
+      quarto::quarto_render(
+        input = "analyses/data_aggregation.qmd",
+        output_format = "all",
+        output_file = paste0("data_aggregation_", s, ".pdf"),
+        execute_params = list(site = s, taper = FALSE)
+      )
+      # move to "outputs/data_aggregation_reports" folder
+      file.rename(
+        from = paste0("data_aggregation_", s, ".pdf"),
+        to = paste0("reports/data_aggregation_", s, ".pdf")
+      )
+    })
+    if(inherits(error, "try-error"))
+      failed_sites <- c(failed_sites, s)
   }
 }
+
+print(paste("The following sites failed:",
+            paste(failed_sites, collapse = ", ")))
 
 # make one file with all sites
 list.files("data/derived_data", "aggregated_data_", full.names = TRUE) |>
   lapply(read.csv) |>
   data.table::rbindlist() |>
   write.csv(file = "data/derived_data/aggregated_data.csv", row.names = FALSE)
-list.files("data/derived_data", "aggregated_data_", full.names = TRUE) |>
-  file.remove()
+list.files("data/derived_data", "aggregated_data_", full.names = TRUE) 
+  #file.remove()
 
 # make one file with all sites - plot area
 list.files("data/derived_data", "plot_area", full.names = TRUE) |>
